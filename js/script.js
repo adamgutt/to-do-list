a0cc4dca0ae112a96d131b2acb59ef1e03b8e13d
adamgutt <93285321+adamgutt@users.noreply.github.com>
2022-03-08 17:24:54 -0500

    fixed error

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function newItem(){
 
-  let li = $(<li></li>);
+  let li = $('<li></li>');
   let inputValue = $('#input').val();
   li.append(inputValue);
 
@@ -14,10 +14,20 @@ function newItem(){
     li.toggleClass("strike");
   }
 
-  li.on("dblclick", crossOut);
+  li.on("dblclick", function crossOut() {
+		li.toggleClass("strike");
+	});
 
   let crossOutButton = $('<crossOutButton></crossOutButton>');
   crossOutButton.append(document.createTextNode('X'));
   li.append(crossOutButton);
 
-}
+  crossOutButton.on("click", deleteListItem);
+
+  function deleteListItem(){
+    li.addClass('delete');
+  }
+
+  $('#list').sortable();
+
+};
